04db2d4ca36bcf2451376610dac4f4ecb656866c
Nick Furlotte <nick.furlotte@gmail.com>
2014-04-07 20:58:07 -0700

    Added the commands to install node manager and get a working node.js

--- setup.sh
+++ setup.sh
@@ -5,6 +5,12 @@
 
 sudo apt-get install -y git
 sudo apt-get install -y curl
+curl https://raw.github.com/creationix/nvm/master/install.sh | sh
+
+source $HOME/.nvm/nvm.sh
+nvm install v0.10.12
+nvm use v0.10.12
+npm install -g jshint
 
 # Install Heroku toolbelt
 # https://toolbelt.heroku.com/debian
