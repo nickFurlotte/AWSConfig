acf8ddd4e681f4327b71a91ad78c11ae49269192
Nick Furlotte <nick.furlotte@gmail.com>
2014-04-07 19:33:52 -0700

    First commit

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Simple setup.sh for configuring Ubuntu 12.04 LTS EC2 instance
+# for headless setup. 
+
+sudo apt-get install -y git
+sudo apt-get install -y curl
+
+# Install Heroku toolbelt
+# https://toolbelt.heroku.com/debian
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
